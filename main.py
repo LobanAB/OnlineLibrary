d7fed51070c3bcb038af091ad1bd7ea0fd991407
Alex <48862843+LobanAB@users.noreply.github.com>
2021-06-09 18:39:05 +0300

    download book covers.

--- main.py
+++ main.py
@@ -3,42 +3,68 @@ from pathlib import Path
 from bs4 import BeautifulSoup
 import os
 from pathvalidate import sanitize_filename
+import urllib
+
 
 def get_book(book_id):
-    url = f"https://tululu.org/txt.php?id={book_id}"
-    print(url)
-    response = requests.get(url)
-    response.raise_for_status()
-    check_for_redirect(response)
-    filename = download_txt(book_id, get_book_header(book_id))
-    with open(filename, 'wb') as file:
-        file.write(response.content)
+    download_txt(book_id)
+    #url = f'https://tululu.org/b{book_id}/'
+    #download_image(url, book_header)
 
 
-def download_txt(book_id, filename, folder='books/'):
+def download_txt(book_id, folder='books/'):
     """Функция для скачивания текстовых файлов.
     Args:
         book_id (str): Cсылка на текст, который хочется скачать.
-        filename (str): Имя файла, с которым сохранять.
         folder (str): Папка, куда сохранять.
     Returns:
         str: Путь до файла, куда сохранён текст.
     """
     # TODO: Здесь ваша реализация
-    return os.path.join(folder, (str(book_id) + '.' + sanitize_filename(filename) + '.txt'))
+    url = f"https://tululu.org/txt.php?id={book_id}"
+    response = requests.get(url)
+    response.raise_for_status()
+    check_for_redirect(response)
+    book_header = get_book_header(book_id)
+    filename = os.path.join(folder, (str(book_id) + '.' + sanitize_filename(book_header['header']) + '.txt'))
+    with open(filename, 'wb') as file:
+        file.write(response.content)
+    download_image(book_header['image'])
 
+
+def download_image(image, folder='images/'):
+    """Функция для скачивания текстовых файлов.
+    Args:
+        image (str): Cсылка на текст, который хочется скачать.
+        folder (str): Папка, куда сохранять.
+    Returns:
+        str: Путь до файла, куда сохранён текст.
+    """
+    url = f'https://tululu.org{image}'
+    print(url)
+    response = requests.get(url)
+    response.raise_for_status()
+    image_name = image.split('/')[-1]
+    filename = os.path.join(folder, image_name)
+    with open(filename, 'wb') as file:
+        file.write(response.content)
 
 def get_book_header(book_id):
     url = f'https://tululu.org/b{book_id}/'
     response = requests.get(url)
     response.raise_for_status()
     soup = BeautifulSoup(response.text, 'lxml')
-    # print(soup.find(class_='bookimage').find('img')['src'])
+    print('Заголовок:', soup.find('h1').text.split('::')[0].strip())
+    print(soup.find(class_='bookimage').find('img')['src'])
+    #print(urllib.parse.urljoin('https://tululu.org', soup.find(class_='bookimage').find('img')['src']))
+    #image = urllib.parse.urljoin('https://tululu.org', soup.find(class_='bookimage').find('img')['src'])
     # print(soup.find_all(class_='d_book')[2].find('td').text)
     # print('Заголовок:', soup.find('h1').text.split('::')[0].strip())
     # print('Автор:', soup.find('h1').text.split('::')[1].strip())
-    book_header = soup.find('h1').text.split('::')[0].strip()
+    book_header = dict(header=soup.find('h1').text.split('::')[0].strip(),
+                       image=soup.find(class_='bookimage').find('img')['src'])
     return book_header
+
 
 def check_for_redirect(response):
     if response.history:
@@ -47,6 +73,7 @@ def check_for_redirect(response):
 
 def main():
     Path("books").mkdir(parents=True, exist_ok=True)
+    Path("images").mkdir(parents=True, exist_ok=True)
     for book_id in range(1, 11):
         try:
             get_book(book_id)
