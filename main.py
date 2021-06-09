85eb120571ba644ac2061511bd440a20748b0966
Alex <48862843+LobanAB@users.noreply.github.com>
2021-06-09 13:45:04 +0300

    download books with titles.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import requests
 from pathlib import Path
-
+from bs4 import BeautifulSoup
+import os
+from pathvalidate import sanitize_filename
 
 def get_book(book_id):
     url = f"https://tululu.org/txt.php?id={book_id}"
@@ -8,10 +10,35 @@ def get_book(book_id):
     response = requests.get(url)
     response.raise_for_status()
     check_for_redirect(response)
-    filename = f'books/id{book_id}.txt'
+    filename = download_txt(book_id, get_book_header(book_id))
     with open(filename, 'wb') as file:
         file.write(response.content)
 
+
+def download_txt(book_id, filename, folder='books/'):
+    """Функция для скачивания текстовых файлов.
+    Args:
+        book_id (str): Cсылка на текст, который хочется скачать.
+        filename (str): Имя файла, с которым сохранять.
+        folder (str): Папка, куда сохранять.
+    Returns:
+        str: Путь до файла, куда сохранён текст.
+    """
+    # TODO: Здесь ваша реализация
+    return os.path.join(folder, (str(book_id) + '.' + sanitize_filename(filename) + '.txt'))
+
+
+def get_book_header(book_id):
+    url = f'https://tululu.org/b{book_id}/'
+    response = requests.get(url)
+    response.raise_for_status()
+    soup = BeautifulSoup(response.text, 'lxml')
+    # print(soup.find(class_='bookimage').find('img')['src'])
+    # print(soup.find_all(class_='d_book')[2].find('td').text)
+    # print('Заголовок:', soup.find('h1').text.split('::')[0].strip())
+    # print('Автор:', soup.find('h1').text.split('::')[1].strip())
+    book_header = soup.find('h1').text.split('::')[0].strip()
+    return book_header
 
 def check_for_redirect(response):
     if response.history:
@@ -25,7 +52,6 @@ def main():
             get_book(book_id)
         except requests.exceptions.HTTPError:
             pass
-
 
 
 if __name__ == '__main__':
