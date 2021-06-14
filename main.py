38f5af1a22859189bbb2dba345f88627da879516
Alex <48862843+LobanAB@users.noreply.github.com>
2021-06-14 12:07:29 +0300

    edit README.md add type hint in main.py

--- main.py
+++ main.py
@@ -5,37 +5,29 @@ import os
 from pathvalidate import sanitize_filename
 import argparse
 
-def get_book(book_id):
-    download_txt(book_id)
 
-
-def download_txt(book_id, folder='books/'):
+def download_txt(book_id: int, folder='books/') -> None:
     """Функция для скачивания текстовых файлов.
     Args:
-        book_id (str): Cсылка на текст, который хочется скачать.
+        book_id (str): id книги, которую хочется скачать.
         folder (str): Папка, куда сохранять.
-    Returns:
-        str: Путь до файла, куда сохранён текст.
     """
-    # TODO: Здесь ваша реализация
     url = f"https://tululu.org/txt.php?id={book_id}"
     response = requests.get(url)
     response.raise_for_status()
     check_for_redirect(response)
     book_page = parse_book_page(book_id)
     filename = os.path.join(folder, (str(book_id) + '.' + sanitize_filename(book_page['header']) + '.txt'))
-    #with open(filename, 'wb') as file:
-    #   file.write(response.content)
+    with open(filename, 'wb') as file:
+        file.write(response.content)
     download_image(book_page['image'])
 
 
-def download_image(image, folder='images/'):
+def download_image(image: str, folder='images/') -> None:
     """Функция для скачивания текстовых файлов.
     Args:
-        image (str): Cсылка на текст, который хочется скачать.
+        image (str): Cсылка на обложку книги, которую хочется скачать.
         folder (str): Папка, куда сохранять.
-    Returns:
-        str: Путь до файла, куда сохранён текст.
     """
     url = f'https://tululu.org{image}'
     response = requests.get(url)
@@ -46,17 +38,12 @@ def download_image(image, folder='images/'):
         file.write(response.content)
 
 
-def parse_book_page(book_id):
+def parse_book_page(book_id: int) -> dict:
     url = f'https://tululu.org/b{book_id}/'
     response = requests.get(url)
     response.raise_for_status()
     soup = BeautifulSoup(response.text, 'lxml')
     print('Заголовок:', soup.find('h1').text.split('::')[0].strip())
-    #print(soup.find(class_='bookimage').find('img')['src'])
-    #print(urllib.parse.urljoin('https://tululu.org', soup.find(class_='bookimage').find('img')['src']))
-    #image = urllib.parse.urljoin('https://tululu.org', soup.find(class_='bookimage').find('img')['src'])
-    # print(soup.find_all(class_='d_book')[2].find('td').text)
-    # print('Заголовок:', soup.find('h1').text.split('::')[0].strip())
     comments = []
     for comment in soup.find(id='content').find_all(class_='black'):
         comments.append(comment.get_text())
@@ -65,16 +52,17 @@ def parse_book_page(book_id):
                        image=soup.find(class_='bookimage').find('img')['src'],
                        comments=comments,
                        author=soup.find('h1').text.split('::')[1].strip(),
-                       genre=soup.find(id='content').find('span', class_='d_book').text.split(':')[1].strip().strip('.').split(', '))
+                       genre=soup.find(id='content').find('span', class_='d_book').text.split(':')[1]
+                       .strip().strip('.').split(', '))
     return book_header
 
 
-def check_for_redirect(response):
+def check_for_redirect(response: requests.models.Response) -> None:
     if response.history:
         raise requests.HTTPError
 
 
-def main():
+def main() -> None:
     parser = argparse.ArgumentParser(description='Программа скачивает книги '
                                                  'с библиотеки tululu.org ')
     parser.add_argument('start_id', help='id книги с которой начать закачку ')
@@ -84,7 +72,7 @@ def main():
     Path("images").mkdir(parents=True, exist_ok=True)
     for book_id in range(int(args.start_id), (int(args.end_id) + 1)):
         try:
-            get_book(book_id)
+            download_txt(book_id)
         except requests.exceptions.HTTPError:
             pass
 
